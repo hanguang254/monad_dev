cbb858b2a831b68c6a1bfa664fac01c815cd7aee
hanguang254 <725204548@qq.com>
2025-02-19 19:39:04 +0800

    renew

--- mian/sahara.py
+++ mian/sahara.py
@@ -7,17 +7,22 @@ from eth_account.messages import encode_defunct
 from rpc_account import RpcConnect
 
 
-def transfer_test():
-    rpc_url = "https://testnet.saharalabs.ai"
-    web3 = RpcConnect().connect_rpc(rpc_url)
 
+
+
+def read_keys():
     # 获取当前脚本所在的目录
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # 构建 CSV 文件的完整路径
     csv_path = os.path.join(current_dir, 'key.csv')
-     # 读取 CSV 文件，跳过标题行，获取第一个密钥
-    keys = RpcConnect().read_key(csv_path,"key")
+    # 读取 CSV 文件，跳过标题行，获取第一个密钥
+    keys = RpcConnect().read_key(csv_path, "key")
+    return keys
 
+
+def transfer_test(key):
+    rpc_url = "https://testnet.saharalabs.ai"
+    web3 = RpcConnect().connect_rpc(rpc_url)
     # 设置转账金额（单位：ETH），并将其转换为 wei（1 ETH = 10^18 wei）
     amount_in_ether = 0.001  # 转账金额（单位：ETH）
     amount_in_wei = web3.to_wei(amount_in_ether, 'ether')
@@ -31,35 +36,32 @@ def transfer_test():
     # print("私钥：", key)
 
     # print(keys)
-    for i in keys:
-        account = RpcConnect().account(web3,key=i)
-        print("地址：",account.address)
-        transaction = {
-            'to': account.address,
-            'value': amount_in_wei,
-            'gas': 25000,  # 设置 gas 限额（可以根据实际情况调整）
-            'gasPrice': gas_price,
-            'nonce': web3.eth.get_transaction_count(account.address),  # 获取当前账户的 nonce
-            'chainId': 313313  # 1 为主网，若是测试网，需要调整
-        }
+    account = RpcConnect().account(web3,key=key)
+    print("地址：",account.address)
+    transaction = {
+        'to': account.address,
+        'value': amount_in_wei,
+        'gas': 25000,  # 设置 gas 限额（可以根据实际情况调整）
+        'gasPrice': gas_price,
+        'nonce': web3.eth.get_transaction_count(account.address),  # 获取当前账户的 nonce
+        'chainId': 313313  # 1 为主网，若是测试网，需要调整
+    }
 
-        # 使用私钥签名交易
-        signed_transaction = web3.eth.account.sign_transaction(transaction, i)
+    # 使用私钥签名交易
+    signed_transaction = web3.eth.account.sign_transaction(transaction, key)
 
-        # 发送已签名的交易
-        tx_hash = web3.eth.send_raw_transaction(signed_transaction.raw_transaction)
-        # 输出交易哈希
-        print(f"交易已发送，交易哈希: {tx_hash.hex()}")
+    # 发送已签名的交易
+    tx_hash = web3.eth.send_raw_transaction(signed_transaction.raw_transaction)
+    # 输出交易哈希
+    print(f"交易已发送，交易哈希: {tx_hash.hex()}")
 
-        # 监听交易确认
-        print("等待交易确认...")
-        tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-        if tx_receipt["status"] == 1:
-            print("发送交易成功")
-            sleep(2)
-            claim(i)
-        else:
-            print(tx_receipt)
+    # 监听交易确认
+    print("等待交易确认...")
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+    if tx_receipt["status"] == 1:
+        print("发送交易成功")
+    else:
+        print(tx_receipt)
 
 
 def claim(key):
@@ -132,13 +134,28 @@ def claim(key):
 
     print(fresh_res)
 
-    if fresh_res == 2 or 3:
+    if int(fresh_res) == 2 or int(fresh_res) == 3:
+        sleep(2)
         url = "https://legends.saharalabs.ai/api/v1/task/claim"
 
         res = requests.post(url=url,headers=headers,json=data)
-        print(res.json())
+        if res.status_code == 200:
+            print(f"领取碎片：{res.json()[0]["amount"]}成功",res.json())
+
+    else:
+        print("已领取")
+
+
+def main():
+    keys = read_keys()
+    for i in keys:
+        # transfer_test(i)
+        claim(i)
 
 
 if __name__ == '__main__':
-    transfer_test()
-    # claim()
+    # load_dotenv()
+    # key = os.getenv("KEY")
+    # print("私钥：", key)
+    # claim(key)
+    main()
